21120493cb187931553e09f4d81d711526bccb28
sforman <sebastian@forman.no>
2022-12-06 20:02:49 +0100

    optimized day 6

--- src/day6/index.test.ts
+++ src/day6/index.test.ts
@@ -1,51 +1,48 @@
-import { vi, describe, it, expect } from "vitest";
-import * as utils from "../utils";
-import { readFileSync } from "fs";
-import { resolve } from "path";
-import { findMarkerIndex, findMessageMarker } from ".";
+import { describe, expect, it } from "vitest";
+import { findMarkerIndex } from ".";
 
 describe("day 6", () => {
   describe("part 1", () => {
     it("can detect marker end position example 1", () => {
-      expect(findMarkerIndex("mjqjpqmgbljsphdztnvjfqwrcgsmlb")).toEqual(7);
+      expect(findMarkerIndex("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4)).toEqual(7);
     });
 
     it("can detect marker end position example 1", () => {
-      expect(findMarkerIndex("bvwbjplbgvbhsrlpgdmjqwftvncz")).toEqual(5);
+      expect(findMarkerIndex("bvwbjplbgvbhsrlpgdmjqwftvncz", 4)).toEqual(5);
     });
 
     it("can detect marker end position example 1", () => {
-      expect(findMarkerIndex("nppdvjthqldpwncqszvftbrmjlhg")).toEqual(6);
+      expect(findMarkerIndex("nppdvjthqldpwncqszvftbrmjlhg", 4)).toEqual(6);
     });
 
     it("can detect marker end position example 1", () => {
-      expect(findMarkerIndex("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")).toEqual(10);
+      expect(findMarkerIndex("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4)).toEqual(10);
     });
 
     it("can detect marker end position example 1", () => {
-      expect(findMarkerIndex("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")).toEqual(11);
+      expect(findMarkerIndex("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4)).toEqual(11);
     });
   });
 
   describe("part 2", () => {
     it("can detect message end position example 1", () => {
-      expect(findMessageMarker("mjqjpqmgbljsphdztnvjfqwrcgsmlb")).toEqual(19);
+      expect(findMarkerIndex("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14)).toEqual(19);
     });
 
     it("can detect message end position example 1", () => {
-      expect(findMessageMarker("bvwbjplbgvbhsrlpgdmjqwftvncz")).toEqual(23);
+      expect(findMarkerIndex("bvwbjplbgvbhsrlpgdmjqwftvncz", 14)).toEqual(23);
     });
 
     it("can detect message end position example 1", () => {
-      expect(findMessageMarker("nppdvjthqldpwncqszvftbrmjlhg")).toEqual(23);
+      expect(findMarkerIndex("nppdvjthqldpwncqszvftbrmjlhg", 14)).toEqual(23);
     });
 
     it("can detect message end position example 1", () => {
-      expect(findMessageMarker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")).toEqual(29);
+      expect(findMarkerIndex("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14)).toEqual(29);
     });
 
     it("can detect message end position example 1", () => {
-      expect(findMessageMarker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")).toEqual(26);
+      expect(findMarkerIndex("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14)).toEqual(26);
     });
-  })
+  });
 });
--- src/day6/index.ts
+++ src/day6/index.ts
@@ -2,17 +2,9 @@ import { loadInput } from "../utils";
 
 const getCharacterList = (input: string, pos: number, len: number) => input.slice(pos - len, pos).split("");
 
-export const findMarkerIndex = (input: string) => {
-  for (let i = 3; i < input.length; i++) {
-    if (new Set(getCharacterList(input, i, 4)).size === 4) {
-      return i;
-    }
-  }
-};
-
-export const findMessageMarker = (input: string) => {
-  for (let i = 14; i < input.length; i++) {
-    if (new Set(getCharacterList(input, i, 14)).size === 14) {
+export const findMarkerIndex = (input: string, length: number) => {
+  for (let i = length - 1; i < input.length; i++) {
+    if (new Set(getCharacterList(input, i, length)).size === length) {
       return i;
     }
   }
@@ -20,9 +12,9 @@ export const findMessageMarker = (input: string) => {
 
 console.log("Day 6");
 const start1 = Date.now();
-console.log("Part 1:", findMarkerIndex(loadInput(6)));
+console.log("Part 1:", findMarkerIndex(loadInput(6), 4));
 console.log("Runtime:", Date.now() - start1);
 console.log("Day 6");
 const start2 = Date.now();
-console.log("Part 2:", findMessageMarker(loadInput(6)));
+console.log("Part 2:", findMarkerIndex(loadInput(6), 14));
 console.log("Runtime:", Date.now() - start2);
