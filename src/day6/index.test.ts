e4fc19b0a01dc20e8c4b6a407b99d9696a3f01ba
sforman <sebastian@forman.no>
2022-12-06 19:56:23 +0100

    day 6 - part 2

--- src/day6/index.test.ts
+++ src/day6/index.test.ts
@@ -2,7 +2,7 @@ import { vi, describe, it, expect } from "vitest";
 import * as utils from "../utils";
 import { readFileSync } from "fs";
 import { resolve } from "path";
-import { findMarkerIndex } from ".";
+import { findMarkerIndex, findMessageMarker } from ".";
 
 describe("day 6", () => {
   describe("part 1", () => {
@@ -26,4 +26,26 @@ describe("day 6", () => {
       expect(findMarkerIndex("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")).toEqual(11);
     });
   });
+
+  describe("part 2", () => {
+    it("can detect message end position example 1", () => {
+      expect(findMessageMarker("mjqjpqmgbljsphdztnvjfqwrcgsmlb")).toEqual(19);
+    });
+
+    it("can detect message end position example 1", () => {
+      expect(findMessageMarker("bvwbjplbgvbhsrlpgdmjqwftvncz")).toEqual(23);
+    });
+
+    it("can detect message end position example 1", () => {
+      expect(findMessageMarker("nppdvjthqldpwncqszvftbrmjlhg")).toEqual(23);
+    });
+
+    it("can detect message end position example 1", () => {
+      expect(findMessageMarker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")).toEqual(29);
+    });
+
+    it("can detect message end position example 1", () => {
+      expect(findMessageMarker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")).toEqual(26);
+    });
+  })
 });
--- src/day6/index.ts
+++ src/day6/index.ts
@@ -1,14 +1,28 @@
 import { loadInput } from "../utils";
 
+const getCharacterList = (input: string, pos: number, len: number) => input.slice(pos - len, pos).split("");
+
 export const findMarkerIndex = (input: string) => {
   for (let i = 3; i < input.length; i++) {
-    if (new Set([input.charAt(i - 3), input.charAt(i - 2), input.charAt(i - 1), input.charAt(i)]).size === 4) {
-      return i + 1;
+    if (new Set(getCharacterList(input, i, 4)).size === 4) {
+      return i;
+    }
+  }
+};
+
+export const findMessageMarker = (input: string) => {
+  for (let i = 3; i < input.length; i++) {
+    if (new Set(getCharacterList(input, i, 14)).size === 14) {
+      return i;
     }
   }
 };
 
 console.log("Day 6");
 const start1 = Date.now();
-console.log("Part 2:", findMarkerIndex(loadInput(6)));
+console.log("Part 1:", findMarkerIndex(loadInput(6)));
 console.log("Runtime:", Date.now() - start1);
+console.log("Day 6");
+const start2 = Date.now();
+console.log("Part 2:", findMessageMarker(loadInput(6)));
+console.log("Runtime:", Date.now() - start2);
